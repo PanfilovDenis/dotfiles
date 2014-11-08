18f19d11271e677cd12f04773358f4d7221a4cda
PanfilovDenis <panf.denis@gmail.com>
2014-11-08 22:54:15 +0300

    mot share zsh hitsory between tabs

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,6 +5,7 @@ SAVEHIST=10000
 setopt EXTENDED_HISTORY
 setopt APPEND_HISTORY
 setopt HIST_IGNORE_ALL_DUPS
+setopt NO_SHARE_HISTORY
 
 bindkey '^[[5D' beginning-of-line
 bindkey '[5D' beginning-of-line
