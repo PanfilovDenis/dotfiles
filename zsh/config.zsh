67a67d87e88108eb625376ebaed53d14ac568287
PanfilovDenis <panf.denis@gmail.com>
2016-02-18 21:37:19 +0300

    update configs

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,9 +5,9 @@ SAVEHIST=10000
 setopt EXTENDED_HISTORY
 setopt APPEND_HISTORY
 setopt HIST_IGNORE_ALL_DUPS
-setopt NO_SHARE_HISTORY
-
+setopt no_share_history
 unsetopt share_history
+unsetopt sharehistory
 
 bindkey '^[[5D' beginning-of-line
 bindkey '[5D' beginning-of-line
